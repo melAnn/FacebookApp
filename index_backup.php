aae9a0fb4469100b389ad71a1b7e2be2f5945390
Melissa Ann Schwarz <schwarz1@corn29.stanford.edu>
2011-01-26 15:30:55 -0800

    New changes to file, changed button and removed excess stuff from example

---
+++ index_backup.php
@@ -0,0 +1,125 @@
+<?php
+
+require_once 'facebook-php-sdk/src/facebook.php';
+
+// Create our Application instance (replace this with your appId and secret).
+$facebook = new Facebook(array(
+  'appId'  => '192979600719639',
+  'secret' => '9c11b647c19ac69732345d6f72954121',
+  'cookie' => true,
+));
+
+// We may or may not have this data based on a $_GET or $_COOKIE based session.
+//
+// If we get a session here, it means we found a correctly signed session using
+// the Application Secret only Facebook and the Application know. We dont know
+// if it is still valid until we make an API call using the session. A session
+// can become invalid if it has already expired (should not be getting the
+// session back in this case) or if the user logged out of Facebook.
+$session = $facebook->getSession();
+
+$me = null;
+// Session based API call.
+if ($session) {
+  try {
+    $uid = $facebook->getUser();
+    $me = $facebook->api('/me');
+    $feed = $facebook->api('/me/feed');
+  } catch (FacebookApiException $e) {
+    error_log($e);
+  }
+}
+
+// login or logout url will be needed depending on current user state.
+if ($me) {
+  $logoutUrl = $facebook->getLogoutUrl();
+} else {
+  $loginUrl = $facebook->getLoginUrl();
+}
+
+// This call will always work since we are fetching public data.
+$naitik = $facebook->api('/naitik');
+
+?>
+<!doctype html>
+<html xmlns:fb="http://www.facebook.com/2008/fbml">
+  <head>
+    <title>Social Study App</title>
+    <style>
+      body {
+        font-family: 'Lucida Grande', Verdana, Arial, sans-serif;
+      }
+      h1 a {
+        text-decoration: none;
+        color: #3b5998;
+      }
+      h1 a:hover {
+        text-decoration: underline;
+      }
+    </style>
+  </head>
+  <body>
+    <!--
+      We use the JS SDK to provide a richer user experience. For more info,
+      look here: http://github.com/facebook/connect-js
+    -->
+    <div id="fb-root"></div>
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId   : '<?php echo $facebook->getAppId(); ?>',
+          session : <?php echo json_encode($session); ?>, // don't refetch the session when PHP already has it
+          status  : true, // check login status
+          cookie  : true, // enable cookies to allow the server to access the session
+          xfbml   : true // parse XFBML
+        });
+      
+	<?php if ($me): ?>
+      (function() {
+        var e = document.createElement('script');
+        e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+        e.async = true;
+        document.getElementById('fb-root').appendChild(e);
+      }());
+    </script>
+  
+	<h2>To participate in this study, click the button below and then click 'ALLOW' </h2>  
+        <fb:login-button perms="read_stream">
+        	Continue
+        </fb:login-button>
+	<?php endif ?>  
+ 
+
+        // whenever the user logs in, we refresh the page
+        FB.Event.subscribe('auth.login', function() {
+          window.location.reload();
+        });
+      };
+
+    <?php if ($me): ?>
+    <a href="<?php echo $logoutUrl; ?>">
+      <img src="http://static.ak.fbcdn.net/rsrc.php/z2Y31/hash/cxrz4k7j.gif">
+    </a>
+    <?php else: ?>
+    <div>
+      Using JavaScript &amp; XFBML: <fb:login-button></fb:login-button>
+    </div>
+    <div>
+      Without using JavaScript &amp; XFBML:
+      <a href="<?php echo $loginUrl; ?>">
+        <img src="http://static.ak.fbcdn.net/rsrc.php/zB6N8/hash/4li2k73z.gif">
+      </a>
+    </div>
+    <?php endif ?>
+
+
+    <h3>Your Facebook Feed</h3>
+    <?php if ($feed): ?>
+    <pre><?php print_r($feed); ?></pre>
+    <?php else: ?>
+    <strong><em>You are not Connected.</em></strong>
+    <?php endif ?>
+    
+
+  </body>
+</html>
